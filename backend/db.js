e3495ff35b7f01a18e627e9b5c260abff191d7a8
Pooja <thanaitpooja09@gmail.com>
2026-08-29 01:38:14 +0530

    fixed the database issue

--- backend/db.js
+++ backend/db.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
+    //password: "Signup@12345",
     password: "",
     database: "signup_db"
 
